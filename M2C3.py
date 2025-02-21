85444face99aa03b4d44766142733b6446d7ce2e
pillajokevin <kevinpillajito@gmail.com>
2025-02-21 16:57:17 -0600

    Carga del ejercicio uno

---
+++ M2C3.py
@@ -0,0 +1,12 @@
+# M2C3-Python-Assignment 
+
+#Exercise 1 
+
+my_string = "Hello I'm Kevin" #Esto es una cadena 
+
+the_number = 28 # Esto es un numero 
+
+my_list = [1 ,2 ,3 ,4, 5 ]
+
+the_boolean = True
+
