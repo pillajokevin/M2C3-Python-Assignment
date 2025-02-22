ed9a76dc2366a331ca9793d15e150feacead5f56
pillajokevin <kevinpillajito@gmail.com>
2025-02-21 18:41:32 -0600

    Abance de los ejercicios

--- M2C3.py
+++ M2C3.py
@@ -10,3 +10,39 @@ my_list = [1 ,2 ,3 ,4, 5 ]
 
 the_boolean = True
 
+# print(my_string)
+# print(the_number)
+# print(my_list)
+# print(the_boolean)
+
+
+#Exercise 2 
+
+first_three_letters = my_string[:3] # Aqui lo estamos guardando en una variable 
+# print(first_three_letters)
+
+
+#Exercise 3 
+
+first_element = my_list[0] # aqui estamos tomando el primer elemento de mi lista 
+# print(first_element)
+
+#Exercise 4 
+
+new_number = 10 
+sum_result = the_number + new_number # Sumandole 10 al numero original
+# print(sum_result)
+
+#Exercise 5 
+
+last_element = my_list[-1] # Hemos usado el negative index 
+# print(last_element)
+
+
+#Exercise 6 
+
+names = "Harry,alex,susie,jared,gail,conner"
+
+list_of_tags = names.split(",")
+
+print(list_of_tags)
