c54daa00c35634b2e10c5ef8cf2b902b6f250b23
pillajokevin <kevinpillajito@gmail.com>
2025-02-22 11:20:12 -0600

    Ejercicios Finalizados

--- Exercise.py
+++ Exercise.py
@@ -2,10 +2,10 @@
 
 string_one = "Hola"
 
-first_word = string_one.find("Hola")
+first_word = string_one.find("Hola")  #Econtrando la palabra con el metodo find 
 
 print(first_word)
 
-sentence = string_one.replace('Hola','Adios')
+sentence = string_one.replace('Hola','Adios') # Remplazamos la palabra Hola , por Adios  
 
 print(sentence)
